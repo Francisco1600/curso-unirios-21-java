a58c9e665257ccded9d50242a286a0c2c90e4742
Francisco1600 <franciscodiniz00@gmail.com>
2021-08-30 15:46:33 -0300

    Curso Spring--Boot-21

---
+++ src/main/java/edu/unirios/projeto/Projeto20212Application.java
@@ -0,0 +1,13 @@
+package edu.unirios.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto20212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto20212Application.class, args);
+	}
+
+}
